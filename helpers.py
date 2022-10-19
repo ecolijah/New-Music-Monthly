88cd0932f581a3642dd7501b14af59534261baa2
ecolijah <56099963+ecolijah@users.noreply.github.com>
2022-10-18 19:17:26 -0500

    Add files via upload

---
+++ helpers.py
@@ -0,0 +1,24 @@
+from datetime import datetime
+from dateutil.relativedelta import relativedelta
+
+
+def isNew(release_date):
+    # subtract a month off of today's time
+    new_date = datetime.today() - relativedelta(months=1)
+    # new_date.strftime('%Y-%M-%D')
+    # print("Date to be older than: " + str(new_date))
+    # format release date obj
+    try:
+        release_date_obj = datetime.strptime(release_date, '%Y-%m-%d')
+    except:
+        print("error for this album date.")
+        return False
+
+    # print("Release Date: " + str(release_date_obj))
+    # perform comparison
+    if new_date < release_date_obj:
+        # print("Its New Music!")
+        return True
+    else:
+        # print("Its Old News..")
+        return False
